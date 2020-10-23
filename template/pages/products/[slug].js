0344901479f0e87533050c62633baeea0ed2e5d7
Bjørge Næss <bjoerge@gmail.com>
2020-10-23 21:34:32 +0200

    Convert to starter template

--- template/pages/products/[slug].js
+++ template/pages/products/[slug].js
@@ -7,40 +7,43 @@ import {getClient, usePreviewSubscription} from '../../utils/sanity'
 const query = groq`*[_type == "product" && slug.current == $slug][0]`
 
 function ProductPageContainer(props) {
-  const {data: productData} = usePreviewSubscription(query, {
-    params: {slug: props?.productData?.slug?.current},
-    initialData: props?.productData,
-    enabled: true
-  })
-  if (props?.errorCode) {
-    return <Error statusCode={errorCode} />
-  }
-  const {
-    _id,
-    title,
-    defaultProductVariant,
-    mainImage,
-    blurb,
-    body,
-    tags,
-    vendor,
-    categories,
-    slug
-  } = productData
-  return (
-    <ProductPage
-      id={_id}
-      title={title}
-      defaultProductVariant={defaultProductVariant}
-      mainImage={mainImage}
-      blurb={blurb}
-      body={body}
-      tags={tags}
-      vendor={vendor}
-      categories={categories}
-      slug={slug?.current}
-    />
-  )
+
+  return <div>Commented out</div>
+  //
+  // const {data: productData} = usePreviewSubscription(query, {
+  //   params: {slug: props?.productData?.slug?.current},
+  //   initialData: props?.productData,
+  //   enabled: true
+  // })
+  // if (props?.errorCode) {
+  //   return <Error statusCode={errorCode} />
+  // }
+  // const {
+  //   _id,
+  //   title,
+  //   defaultProductVariant,
+  //   mainImage,
+  //   blurb,
+  //   body,
+  //   tags,
+  //   vendor,
+  //   categories,
+  //   slug
+  // } = productData
+  // return (
+  //   <ProductPage
+  //     id={_id}
+  //     title={title}
+  //     defaultProductVariant={defaultProductVariant}
+  //     mainImage={mainImage}
+  //     blurb={blurb}
+  //     body={body}
+  //     tags={tags}
+  //     vendor={vendor}
+  //     categories={categories}
+  //     slug={slug?.current}
+  //   />
+  // )
 }
 
 export async function getStaticPaths() {
--- template/studio/README.md
+++ template/studio/README.md
@@ -1,4 +1,4 @@
-# Sanity Ecommerce Content Studio
+# Sanity E-commerce Content Studio
 
 Congratulations, you have now installed the Sanity Content Studio, an open source real-time content editing environment connected to the Sanity backend.
 
